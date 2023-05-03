e526ca55d63676e804b9c41988b7744bab79d71d
aarticontractor <contaaru@gmail.com>
2023-05-02 20:41:08 -0700

    defined columns of DB models with requirements

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,12 +7,30 @@ const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
-ProductTag.init(
-  {
-  // define columns
-  
+ProductTag.init({
+  // define columns with database requirements
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
+  product_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'product',
+      key: 'id',
+    },
+  },
+  tag_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'tag',
+      key: 'id',
+    },
+  },
+
+}, {
   sequelize,
   timestamps: false,
   freezeTableName: true,
@@ -21,4 +39,4 @@ ProductTag.init(
 });
 
 
-    module.exports = ProductTag;
+module.exports = ProductTag;
